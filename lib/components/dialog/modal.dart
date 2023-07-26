3d62a84994880fa8afbef476e8d40da75442b87a
Nishant <nishantyt0@gmail.com>
2023-07-26 16:19:04 +0545

    feat : added basic modal

---
+++ lib/components/dialog/modal.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class Modal extends StatelessWidget {
+  final String title;
+  final String message;
+  final Function()? onConfirm;
+  final Function()? onCancel;
+
+  const Modal({
+    required this.title,
+    required this.message,
+    this.onConfirm,
+    this.onCancel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(message),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                if (onCancel != null)
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      if (onCancel != null) onCancel!();
+                    },
+                    child: Text('Cancel'),
+                  ),
+                if (onConfirm != null)
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      if (onConfirm != null) onConfirm!();
+                    },
+                    child: Text('Confirm'),
+                  ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
